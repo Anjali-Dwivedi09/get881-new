eee1f6ea046eb81981cfa84b27570603dedfff5c
Anjali-Dwivedi09 <anjalidwivedi0109@gmail.com>
2020-12-18 16:29:01 +0530

    altered sum

--- Demo2/src/com/lti/model/Calc.java
+++ Demo2/src/com/lti/model/Calc.java
@@ -2,7 +2,7 @@ package com.lti.model;
 
 public class Calc {
 	public void add(int n, int m){
-		System.out.println("sum="+(n+m));
+		System.out.println("summation="+(n+m));
 		}
 	
 	public void diff(int n, int m){
